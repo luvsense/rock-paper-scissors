6c473b1857bd2c5435279a29d8b58f928094a2f5
luvsense <yvsh7.btw@gmail.com>
2024-03-09 23:01:59 +0530

    fixed fucntion call issues

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
-console.log("Hello World");
-
 function getComputerChoice() {
   // Randomly returns either Rock, Paper, Scissor.
-  const choices = ["rock", "paper", "scissors"];
+  const choices = ["rock", "paper", "scissor"];
   const chance = Math.floor(Math.random() * choices.length);
-  console.log(choices[chance]);
-}
-function getplayerSelection(playerSelection) {
-  // Takes the player selection for the round.
-}
-function playRound(getComputerChoice, getplayerSelection) {
-  // Plays a single round of Rock, Paper, Scissor.
+  return choices[chance];
 }
 
-getplayerSelection("Rock");
-getComputerChoice();
+function playRound(computerSelection, playerSelection) {
+  // Plays a single round of Rock, Paper, Scissor.
+  if (computerSelection === playerSelection) {
+    console.log("It's a TIE!");
+  } else if (
+    (computerSelection === "rock" && playerSelection === "scissor") ||
+    (computerSelection === "paper" && playerSelection === "rock") ||
+    (computerSelection === "scissor" && playerSelection === "paper")
+  ) {
+    console.log("Computer Wins!");
+  } else {
+    console.log("Player Wins!");
+  }
+}
+
+const playerSelection = prompt(
+  `Choose: rock, paper, scissor`,
+  ""
+).toLowerCase();
+
+const computerSelection = getComputerChoice();
+console.log(`Computer Played: ${computerSelection}`);
+console.log(`Played Played: ${playerSelection}`);
+playRound(computerSelection, playerSelection);
