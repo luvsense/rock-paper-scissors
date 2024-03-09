e902331f1b584bb1f17749ccbaf0422377bedfdd
luvsense <yvsh7.btw@gmail.com>
2024-03-09 21:24:18 +0530

    computer choice randomizer implementation

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World");
+
+function getComputerChoice() {
+  // Randomly returns either Rock, Paper, Scissor.
+  const choices = ["rock", "paper", "scissors"];
+  const chance = Math.floor(Math.random() * choices.length);
+  console.log(choices[chance]);
+}
+function getplayerSelection(playerSelection) {
+  // Takes the player selection for the round.
+}
+function playRound(getComputerChoice, getplayerSelection) {
+  // Plays a single round of Rock, Paper, Scissor.
+}
+
+getplayerSelection("Rock");
+getComputerChoice();
