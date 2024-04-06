60de9a3fa5ce10e6222556d4c2e19a1a77785284
luvsense <yvsh7.btw@gmail.com>
2024-04-06 15:48:20 +0530

    UI Added

--- script.js
+++ script.js
@@ -1,36 +1,89 @@
+const rockBtn = document.querySelector('.rockbtn');
+const paperBtn = document.querySelector('.paperbtn');
+const scissorBtn = document.querySelector('.scissorbtn');
+const result = document.querySelector('.result');
+const playBtn = document.querySelectorAll('.playbtn');
+const displayRounds = document.querySelector('.displayRounds');
+const resetGame = document.querySelector('.resetGame');
+let playerScore = 0;
+let computerScore = 0;
+let totalRounds = 0;
+playGame = true;
+
+function disableButtons() {
+  for (let btn of playBtn) {
+    btn.disabled = true;
+  }
+}
+
+function checkGameStaus() {
+  if (totalRounds >= 5) {
+    playGame = false;
+    disableButtons();
+  }
+}
+
+if (!playGame) {
+  disableButtons();
+} else {
+  rockBtn.addEventListener('click', function () {
+    playRound('rock');
+  });
+
+  paperBtn.addEventListener('click', function () {
+    playRound('paper');
+  });
+
+  scissorBtn.addEventListener('click', function () {
+    playRound('scissor');
+  });
+}
+
 function getComputerChoice() {
   // Randomly returns either Rock, Paper or Scissor.
-  const choices = ["rock", "paper", "scissor"];
+  const choices = ['rock', 'paper', 'scissor'];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
 
-function playRound(computerSelection, playerSelection) {
+function playRound(playerSelection) {
+  if (!playGame) return;
+  const computerSelection = getComputerChoice();
+
+  if (totalRounds >= 5) {
+    playGame = false;
+    disableButtons();
+    return;
+  }
   // Plays a single round of Rock, Paper, Scissor.
   if (computerSelection === playerSelection) {
+    result.textContent = "It's a TIE";
     console.log("It's a TIE!");
   } else if (
-    (computerSelection === "rock" && playerSelection === "scissor") ||
-    (computerSelection === "paper" && playerSelection === "rock") ||
-    (computerSelection === "scissor" && playerSelection === "paper")
+    (computerSelection === 'rock' && playerSelection === 'scissor') ||
+    (computerSelection === 'paper' && playerSelection === 'rock') ||
+    (computerSelection === 'scissor' && playerSelection === 'paper')
   ) {
-    console.log("Computer Wins!");
+    console.log('Computer Wins!');
+    result.textContent = 'Computer Wins!';
+    computerScore += 1;
   } else {
-    console.log("Player Wins!");
+    console.log('Player Wins!');
+    result.textContent = 'Player Wins!';
+    playerScore += 1;
   }
+  totalRounds++;
+  displayRounds.textContent = `Round : ${totalRounds}`;
+  checkGameStaus();
 }
 
-// Prompts user for an Input and converts it to lowercase
-const playerSelection = prompt(
-  `Choose: rock, paper, scissor`,
-  ""
-).toLowerCase();
-
-// Holds computer selection value returned by fucntion in a constant.
-const computerSelection = getComputerChoice();
-
-console.log(`Computer Played: ${computerSelection}`);
-console.log(`Player Played: ${playerSelection}`);
-
-// Executes one round of game.
-playRound(computerSelection, playerSelection);
+resetGame.addEventListener('click', function () {
+  playerScore = 0;
+  computerScore = 0;
+  totalRounds = 0;
+  playGame = true;
+  for (let btn of playBtn) {
+    btn.disabled = false;
+  }
+  displayRounds.textContent = `Round : ${totalRounds}`;
+});
